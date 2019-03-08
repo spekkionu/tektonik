30034b2bf094561d1c36870ce04d8c9cf2112318
Jonathan Bernardi <jon@jonbernardi.com>
2019-03-07 22:04:14 -0800

    update version and php requirements

--- tektonik.php
+++ tektonik.php
@@ -4,13 +4,14 @@
  * Plugin URI:  https://github.com/spekkionu/tektonik
  * GitHub Plugin URI: spekkionu/tektonik
  * Description: PHP Template Engine for WordPress
- * Version:     20170405
+ * Version:     20190307
  * Author:      Jonathan Bernardi
  * Author URI:  https://developer.wordpress.org/
  * GitHub Plugin URI: spekkionu/tektonik
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: tektonik
+ * Requires PHP:      5.6
  *
  * @package Tektonik
  */
